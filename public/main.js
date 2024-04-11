bb70b77131607c9817d11133b1bf7b484b515175
Shutiak Vitalii <122994426+juniorvitalik997@users.noreply.github.com>
2024-04-11 13:35:57 +0300

    Created a homepage and adapted it for different devices

--- public/main.js
+++ public/main.js
@@ -1,1 +1,28 @@
-console.log('work');
+$("#burger").click(function () {
+    $("header").css({
+      height: "35vh",
+      "flex-direction": "column",
+      "justify-content": "space-between",
+      display: "flex",
+      backdropFilter: 'blur(1px)',
+      backgroundColor: 'rgba(0, 0, 0, 0.9)'
+    });
+    $("ul").css({
+      "flex-direction": "column",
+      display: "flex",
+    });
+    $("li").css("margin-top", "20px");
+    $("#burger").css("display", "none");
+    $("#exitBurger").css("display", "flex");
+  });
+  
+  $("#exitBurger").click(function () {
+    $("header").css({
+      height: "10vh",
+      "flex-direction": "row",
+      "justify-content": "space-between;",
+      backgroundColor: 'transparent' // Видаляємо бекграунд
+    });
+    $("#burger").css("display", "flex");
+    $("#exitBurger, ul").css("display", "none");
+  });
